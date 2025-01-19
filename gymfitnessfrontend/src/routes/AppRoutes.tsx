1fb6543c6cd1c2ac7749d808bbb7aab7021e0b0c
abrahamraies <abrahamraies@gmail.com>
2025-01-19 20:15:11 +1000

    Add prettier and eslint

--- gymfitnessfrontend/src/routes/AppRoutes.tsx
+++ gymfitnessfrontend/src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
 // import Home from "../features/Home/Home";
 // import Test from "../features/Test/Test";
 // import Dashboard from "../features/Dashboard/Dashboard";
