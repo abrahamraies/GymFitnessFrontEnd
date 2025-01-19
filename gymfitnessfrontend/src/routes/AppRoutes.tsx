259d717d9dfe22df25cd9d574ad623133b561a3f
abrahamraies <abrahamraies@gmail.com>
2025-01-19 20:02:17 +1000

    first commit

---
+++ gymfitnessfrontend/src/routes/AppRoutes.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "../features/Home/Home";
+// import Test from "../features/Test/Test";
+// import Dashboard from "../features/Dashboard/Dashboard";
+
+function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/dashboard" element={<Dashboard />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRoutes;
